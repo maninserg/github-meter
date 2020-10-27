347d944d7d63b89025019ea7f33bfedfb4a54b29
maninserg <manin.serg@gmail.com>
2020-10-27 11:39:35 +0300

    Add control menu of program

--- gh_meter.py
+++ gh_meter.py
@@ -4,12 +4,34 @@
 
 import gh_meter_function as fn
 
-print ("\n" * 100)
 
-total_dict = fn.create_total_dict()
-
-data_count_sorted = fn.process_total_dict(total_dict)
-
-fn.create_total_count_table(data_count_sorted)
-
-fn.create_total_count_chart(data_count_sorted)
+while True:
+    print ("\n" * 100)
+    print ("--------------------------------")
+    print ("Main menu program github-meter")
+    print ("--------------------------------")
+    print ("")
+    print ("1.Show pop languages on GitHub")
+    print ("0.Exit")
+    print ("")
+    key=input("Your choice: ")
+    if key == '0':
+        break
+    elif key == '1':
+        print ("\n" * 100)
+        total_dict = fn.create_total_dict()
+        data_count_sorted = fn.process_total_dict(total_dict)
+        print ("\n" * 100)
+        fn.create_total_count_table(data_count_sorted)
+        fn.create_total_count_chart(data_count_sorted)
+        print("")
+        print("1.Back to Main menu")
+        print("0.Exit")
+        print("")
+        key=input("Your choice: ")
+        if key == '0':
+            break
+        elif key == '1':
+            continue
+        else:
+            key=input("Your choice: ")
